7b824213a3226b9431a2d656b2c5322e5ee1e13e
pretoniano <vicgabriel.bahia@gmail.com>
2024-03-08 00:09:59 -0300

    feat: configurando o userControler

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
+import { serviceCreate } from "../services/serviceCreate";
 
 class createUser {
   create(req: Request, res: Response) {
     console.log("acessado");
+    const { name, email } = req.body as { name: string; email: string };
 
-    res.json({ message: "criado" });
+    const service = new serviceCreate();
+
+    const serviceResponse = service.executar({name, email});
+
+    res.send(serviceResponse);
   }
 }
 
